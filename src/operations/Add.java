55b6f209f752ac26934ab7fa4e7c44e801889777
Santosh Dubey <santoshdubey418@gmail.com>
2021-12-29 15:55:56 +0530

    initial version

---
+++ src/operations/Add.java
@@ -0,0 +1,17 @@
+package operations;
+import java.util.*;
+public class Add {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int a;
+		int b;
+		int c;
+		System.out.println("Enter the values of a and b");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=a+b;
+		System.out.println(c);
+	}
+
+}
